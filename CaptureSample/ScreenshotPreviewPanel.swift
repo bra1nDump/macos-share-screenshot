b6d209c78fa3815c677281078b2b8bd3865a65b2
Oleg <yakushinoa@gmail.com>
2024-01-16 01:15:39 +0300

    Watched how it works with two monitors (it doesn't); when selecting an area on the second screen, a screenshot of the primary display is taken (learning how to fix this). Fixed the bug with onHover. Adjusted the logic of functions. Hid the application from the dock since we don't need it there. Window magic is still in progress. Noticed that the status bar is not displayed, or is it just my issue?

--- CaptureSample/ScreenshotPreviewPanel.swift
+++ CaptureSample/ScreenshotPreviewPanel.swift
@@ -9,12 +9,18 @@
 import SwiftUI
 
 class ScreenshotPreviewPanel: NSPanel {
+    override var canBecomeKey: Bool {
+            return true
+        }
     init(imageData: [ImageData]) {
-        let previewRect = NSRect(x: 0, y: 0, width: 300, height: 950)
+        let previewRect = NSRect(x: -20, y: 0, width: 300, height: 950)
         super.init(contentRect: previewRect, styleMask: .borderless, backing: .buffered, defer: false)
         self.backgroundColor = NSColor.clear
+        self.isFloatingPanel = true
+        self.worksWhenModal = true
         self.isOpaque = false
-        self.level = .floating
+        self.level = .mainMenu
+        self.collectionBehavior = [.canJoinAllSpaces, .fullScreenAuxiliary]
         let hostingView = NSHostingView(rootView: CaptureStackView(capturedImages: imageData))
         hostingView.frame = previewRect
         self.contentView?.addSubview(hostingView)
--- CaptureSample/Views/ScreenShotView.swift
+++ CaptureSample/Views/ScreenShotView.swift
@@ -7,10 +7,11 @@
 //
 
 import SwiftUI
+import AppKit
 
 struct ScreenShotView: View {
     var image: ImageData
-    @State var isHovered = false
+    @State private var isHovered = false
     var saveImage: ((ImageData) -> Void)
     var copyImage: ((ImageData) -> Void)
     var deleteImage: ((ImageData) -> Void)
@@ -21,85 +22,98 @@ struct ScreenShotView: View {
                 .frame(width: 200, height: 150)
                 .background(Color.clear)
                 .cornerRadius(10)
-                .rotationEffect(.degrees(180))
                 .draggable(Image(nsImage: NSImage(data: image)!))
+                .rotationEffect(.degrees(180))
                 .overlay(
                         RoundedRectangle(cornerRadius: 10)
                             .stroke(Color.white, lineWidth: 1)
-                            .overlay(
-                                ZStack{
-                                    VStack{
-                                        HStack{
-                                            Circle()
-                                                .frame(width: 25, height: 25)
-                                                .foregroundColor(.white)
-                                                .overlay(
-                                                    Image(systemName: "xmark")
-                                                        .foregroundColor(.black)
-                                                )
-                                                .onTapGesture {
-                                                    deleteImage(image)
-                                                }
-                                            Spacer()
-                                            Circle()
-                                                .frame(width: 25, height: 25)
-                                                .foregroundColor(.white)
-                                                .overlay(
-                                                    Image(systemName: "pin.fill")
-                                                        .foregroundColor(.black)
-                                                        .rotationEffect(.degrees(45))
-                                                )
-                                        }
-                                        Spacer()
-                                        HStack{
-                                            Circle()
-                                                .frame(width: 25, height: 25)
-                                                .foregroundColor(.white)
-                                                .overlay(
-                                                    Image(systemName: "pencil")
-                                                        .foregroundColor(.black)
-                                                )
-                                            Spacer()
-                                            Circle()
-                                                .frame(width: 25, height: 25)
-                                                .foregroundColor(.white)
-                                                .overlay(
-                                                    Image(systemName: "icloud.and.arrow.up.fill")
-                                                        .foregroundColor(.black)
-                                                )
-                                        }
-                                    }
-                                    .padding(5)
-                                    VStack{
-                                        RoundedRectangle(cornerRadius: 20)
-                                            .frame(width: 50, height: 25)
-                                            .foregroundColor(.white)
-                                            .overlay(
-                                                Text("Copy")
-                                                    .foregroundColor(.black)
-                                            )
-                                            .onTapGesture {
-                                                copyImage(image)
-                                            }
-                                        RoundedRectangle(cornerRadius: 20)
-                                            .frame(width: 50, height: 25)
-                                            .foregroundColor(.white)
-                                            .overlay(
-                                                Text("Save")
-                                                    .foregroundColor(.black)
-                                            )
-                                            .onTapGesture {
-                                                saveImage(image)
-                                            }
-                                    }
-                                }
-                            )
                             .rotationEffect(.degrees(180))
                             .opacity(isHovered ? 1.0 : 0.0)
                 )
-                .onHover { hovering in
-                                   isHovered = hovering
-                               }
+                .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color.clear)
+                                .frame(width: 195, height: 145)
+                                .overlay(
+                                    ZStack{
+                                        VStack{
+                                            HStack{
+                                                Circle()
+                                                    .frame(width: 25, height: 25)
+                                                    .foregroundColor(.white)
+                                                    .overlay(
+                                                        Image(systemName: "xmark")
+                                                            .foregroundColor(.black)
+                                                    )
+                                                    .onTapGesture {
+                                                        deleteImage(image)
+                                                    }
+                                                Spacer()
+                                                Circle()
+                                                    .frame(width: 25, height: 25)
+                                                    .foregroundColor(.white)
+                                                    .overlay(
+                                                        Image(systemName: "pin.fill")
+                                                            .foregroundColor(.black)
+                                                            .rotationEffect(.degrees(45))
+                                                    )
+                                            }
+                                            Spacer()
+                                            HStack{
+                                                Circle()
+                                                    .frame(width: 25, height: 25)
+                                                    .foregroundColor(.white)
+                                                    .overlay(
+                                                        Image(systemName: "pencil")
+                                                            .foregroundColor(.black)
+                                                    )
+                                                Spacer()
+                                                Circle()
+                                                    .frame(width: 25, height: 25)
+                                                    .foregroundColor(.white)
+                                                    .overlay(
+                                                        Image(systemName: "icloud.and.arrow.up.fill")
+                                                            .foregroundColor(.black)
+                                                    )
+                                            }
+                                        }
+                                        .padding(5)
+                                        VStack{
+                                            RoundedRectangle(cornerRadius: 20)
+                                                .frame(width: 50, height: 25)
+                                                .foregroundColor(.white)
+                                                .overlay(
+                                                    Text("Copy")
+                                                        .foregroundColor(.black)
+                                                )
+                                                .onTapGesture {
+                                                    copyImage(image)
+                                                }
+                                            RoundedRectangle(cornerRadius: 20)
+                                                .frame(width: 50, height: 25)
+                                                .foregroundColor(.white)
+                                                .overlay(
+                                                    Text("Save")
+                                                        .foregroundColor(.black)
+                                                )
+                                                .onTapGesture {
+                                                    saveImage(image)
+                                                }
+                                        }
+                                    }
+                                        .rotationEffect(.degrees(180))
+                                        .opacity(isHovered ? 1.0 : 0.0)
+                                )
+                                .onHover { hovering in
+                                    withAnimation {
+                                        isHovered = hovering
+                                    }
+                                }
+                                .draggable(Image(nsImage: NSImage(data: image)!))
+                            
+                              
+                        )
                 .focusable(false)
+               
             }
 }
