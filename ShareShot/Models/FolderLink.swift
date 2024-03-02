fa28c9c21b94c77ea3b6a3ca374d6620af8704dc
Oleg <yakushinoa@gmail.com>
2024-03-03 02:42:37 +0300

    Commit 02.03 Model for storing URLs of recent folders Working on an image editing window

---
+++ ShareShot/Models/FolderLink.swift
@@ -0,0 +1,52 @@
+//
+//  FolderLink.swift
+//  ShareShot
+//
+//  Created by Oleg Yakushin on 3/3/24.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import Foundation
+
+struct FolderLink: Codable {
+    var name: String
+    var url: URL
+}
+
+class FolderManager {
+    private var recentFolders: [FolderLink] = []
+    private let maxRecentFoldersCount = 3
+    func addFolderLink(name: String, url: URL) {
+        let newLink = FolderLink(name: name, url: url)
+        if let existingIndex = recentFolders.firstIndex(where: { $0.url == url }) {
+            recentFolders.remove(at: existingIndex)
+        }
+        recentFolders.insert(newLink, at: 0)
+        if recentFolders.count > maxRecentFoldersCount {
+            recentFolders.removeLast()
+        }
+        saveToUserDefaults()
+    }
+    func getRecentFolders() -> [FolderLink] {
+        return recentFolders
+    }
+    private func saveToUserDefaults() {
+        do {
+            let encoder = JSONEncoder()
+            let data = try encoder.encode(recentFolders)
+            UserDefaults.standard.set(data, forKey: "recentFolders")
+        } catch {
+            print("Failed to save recent folders to UserDefaults: \(error)")
+        }
+    }
+    func loadFromUserDefaults() {
+        if let data = UserDefaults.standard.data(forKey: "recentFolders") {
+            do {
+                let decoder = JSONDecoder()
+                recentFolders = try decoder.decode([FolderLink].self, from: data)
+            } catch {
+                print("Failed to load recent folders from UserDefaults: \(error)")
+            }
+        }
+    }
+}
---
+++ ShareShot/Views/ImageEditorWindow.swift
@@ -0,0 +1,78 @@
+//
+//  ImageEditorWindow.swift
+//  ShareShot
+//
+//  Created by Oleg Yakushin on 3/3/24.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class ImageEditorWindowController: NSWindowController, NSWindowDelegate {
+    private var imageView: NSImageView!
+
+    convenience init() {
+        let window = NSWindow(contentRect: NSRect(x: 0, y: 0, width: 600, height: 400),
+                              styleMask: [.closable, .resizable, .miniaturizable, .fullSizeContentView],
+                              backing: .buffered,
+                              defer: false)
+        self.init(window: window)
+
+        window.title = "Image Editor"
+        window.delegate = self
+        window.center()
+
+        setupUI()
+    }
+
+    private func setupUI() {
+        imageView = NSImageView(frame: NSRect(x: 20, y: 20, width: 400, height: 300))
+        imageView.imageScaling = .scaleProportionallyUpOrDown
+        imageView.imageAlignment = .alignCenter
+        window?.contentView?.addSubview(imageView)
+        let openButton = NSButton(title: "Open Image", target: self, action: #selector(openImage(_:)))
+        openButton.frame = NSRect(x: 440, y: 250, width: 140, height: 30)
+        window?.contentView?.addSubview(openButton)
+        let saveButton = NSButton(title: "Save Image", target: self, action: #selector(saveImage(_:)))
+        saveButton.frame = NSRect(x: 440, y: 200, width: 140, height: 30)
+        window?.contentView?.addSubview(saveButton)
+    }
+
+    @objc private func openImage(_ sender: Any) {
+        let openPanel = NSOpenPanel()
+        openPanel.allowedFileTypes = ["jpg", "jpeg", "png"]
+
+        openPanel.begin { response in
+            if response == .OK, let imageURL = openPanel.url {
+                let image = NSImage(contentsOf: imageURL)
+                self.imageView.image = image
+            }
+        }
+    }
+
+    @objc private func saveImage(_ sender: Any) {
+        guard let image = imageView.image else {
+            // Handle the case when there is no image
+            return
+        }
+
+        let savePanel = NSSavePanel()
+        savePanel.allowedFileTypes = ["png", "jpg", "jpeg"]
+        savePanel.begin { response in
+            if response == .OK, let saveURL = savePanel.url {
+                guard let data = image.tiffRepresentation else {
+                    // Handle the case when there is an issue with image representation
+                    return
+                }
+
+                do {
+                    try data.write(to: saveURL, options: .atomic)
+                } catch {
+                    // Handle the error while saving
+                    print("Error saving image: \(error)")
+                }
+            }
+        }
+    }
+}
